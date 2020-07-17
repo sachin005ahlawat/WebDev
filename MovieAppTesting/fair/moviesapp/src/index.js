14cfc735e3d5810450a5899282b8e8b470f20725
Sachin Ahlawat <sachin.ahlawat101@gmail.com>
2020-07-18 02:38:07 +0530

    commit

---
+++ MovieAppTesting/fair/moviesapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import MoviesPage from "./homePage"
+import App from "./app";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <BrowserRouter>
+    <App></App>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
